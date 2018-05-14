22d31de28ef47471bb83a345e9ca18f2b4f0bc7d
Paulo Nikko Peral <thelostsheeplefindshope@gmail.com>
2018-05-13 21:42:00 -0700

    Lesson 2 Complete

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -2,7 +2,7 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Contact} from "../contact.model";
 
 @Component({
-  selector: 'app-contact-list',
+  selector: 'app-contacts-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
